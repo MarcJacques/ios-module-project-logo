191bbc3b6235dcd6f6b7bf5bbdcb2be07cc7cdb9
Marc Jacques <Marcjacques@me.com>
2019-09-11 16:22:40 -0400

    just setup x-code

---
+++ ProjectLogo/ProjectLogo/Logo.swift
@@ -0,0 +1,12 @@
+//
+//  Logo.swift
+//  ProjectLogo
+//
+//  Created by Marc Jacques on 9/11/19.
+//  Copyright © 2019 Marc Jacques. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
---
+++ ProjectLogo/ProjectLogo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProjectLogo
+//
+//  Created by Marc Jacques on 9/11/19.
+//  Copyright © 2019 Marc Jacques. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
